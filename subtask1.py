36c1ce18d7b108ffd5eca04ca31ba73fa6f77031
Pedro Duschitz <s2410769019@fhooe.at>
2024-11-25 10:05:40 +0100

    1

---
+++ subtask1.py
@@ -0,0 +1,7 @@
+n = 23
+q = 0
+while n>=(q*q):
+    q +=1
+    print(q)
+q = q-1
+print(n,q*q)
---
+++ subtask2.py
@@ -0,0 +1,16 @@
+x = [3,4,0,2,1,8,-1]
+n=0
+s=0
+m=0
+a=0
+
+while x[n] != -1:
+    s+=x[n]
+    
+    if m>x[n]:
+        m=x[n]
+    n+=1
+
+a = s/n
+
+print (n,s,m,a)
